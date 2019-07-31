9040abbd72115f05a96dffb51ffa2ce30b06d76c
ramadhanafif <ramadhanafif@gmail.com>
2019-07-31 15:35:45 +0700

    modified:   XST_GPIO/Examples/blink_and_read_test.cpp

--- XST_GPIO/Examples/blink_and_read_test.cpp
+++ XST_GPIO/Examples/blink_and_read_test.cpp
@@ -1,1 +1,37 @@
-#!/usr/bin/env wolframscript
+#include "XST_GPIO.h"
+#include "utility.h"
+#include "lcd_xirka.h"
+
+#define BLINKTEST
+#define READTEST
+
+XST_GPIO x(GPIO0);
+XST_GPIO y(GPIO1);
+
+void blinkled(int pin, XST_GPIO gpio){
+	while(1){
+		gpio.digitalWrite(pin,HIGH);
+		delay(100);
+		gpio.digitalWrite(pin,LOW);
+		delay(100);
+	}
+}
+
+
+int main(){
+#ifdef BLINKTEST
+	y.pinMode(3,OUTPUT);
+	blinkled(3,y);
+#endif
+
+#ifdef READTEST	
+	int pin = 0;
+	LCD_init();
+	x.pinMode(pin,INPUT);
+	while(1){
+		if(x.digitalRead(pin)){
+			LCD_WriteString("IN");
+		}
+	}
+#endif
+}
