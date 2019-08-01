3d25080560a87a8ed41515982b064af141d5beae
ramadhanafif <ramadhanafif@gmail.com>
2019-08-01 13:18:30 +0700

    modified:   XST_I2C/Examples/I2C_BME_TEST.cpp

--- XST_I2C/Examples/I2C_BME_TEST.cpp
+++ XST_I2C/Examples/I2C_BME_TEST.cpp
@@ -1,3 +1,7 @@
+#include "BME280.h"
+#include "XST_UART.h"
+#include "XST_I2C.h"
+
 // Variable Declarations
 volatile uint32_t i2c_irq_triggered = 0;
 uint8_t dataToWrite = 0;
@@ -13,7 +17,7 @@ int var1,var2,t_fine;
 #define addr 0x77
 
 XST_UART u(UART2);
-XST_I2C i(BME280);
+XST_I2C bme(BME280);
 
 int main(void)
 {
@@ -23,7 +27,7 @@ int main(void)
 	
     //I2C init
 	u.println("Initiliaze I2C");
-	if (i.init (25000000, 1) == I2C_ERROR) 
+	if (bme.init (25000000, 1) == I2C_ERROR) 
         return I2C_ERROR;
 	
     //test procedures
@@ -51,7 +55,7 @@ float jadiTempC(int32_t adc_T, int32_t T1, int32_t T2, int32_t T3 ){
 void read_sensor_test(void){
 	u.println("BME280 Sensor Test");
 	u.println("Sensor Configuration Begin");
-	i.write(addr, BME280_CTRL_MEAS_REG, 0x00);
+	bme.write(addr, BME280_CTRL_MEAS_REG, 0x00);
 	runMode = 3;
 	tStandby = 0;
 	filter = 0;
@@ -62,11 +66,11 @@ void read_sensor_test(void){
 	
     dataToWrite = (tStandby << 0x05) & 0xE0;
 	dataToWrite |= (filter << 0x02) & 0x1C;
-	i.write(addr, BME280_CONFIG_REG, dataToWrite);
+	bme.write(addr, BME280_CONFIG_REG, dataToWrite);
 	
     //set ctrl_hum first, then ctrl_meas to activate ctrl_hum
 	dataToWrite = humidOverSample & 0x07;
-	i.write(addr, BME280_CTRL_HUMIDITY_REG, dataToWrite);
+	bme.write(addr, BME280_CTRL_HUMIDITY_REG, dataToWrite);
 	
     //set ctrl_meas
 	//First, set temp oversampling
@@ -76,26 +80,26 @@ void read_sensor_test(void){
 	//Last, set mode
 	dataToWrite |= (runMode) & 0x03;
 	//Load the byte
-	i.write(addr, BME280_CTRL_MEAS_REG, dataToWrite);
+	bme.write(addr, BME280_CTRL_MEAS_REG, dataToWrite);
 	u.println("Sensor Configuration Done");
 	u.println("Begin Reading Registers");
 	
-    u.print("ID (0xD0): 0x"); u.println(i.read(0x77, BME280_CHIP_ID_REG),HEX);
-	u.print("Reset register (0xE0): 0x"); u.println(i.read(0x77, BME280_RST_REG),HEX);
-	u.print("ctrl_meas (0xF4): 0x"); u.println(i.read(0x77, BME280_CTRL_MEAS_REG),HEX);
-	u.print("ctrl_hum (0xF2): 0x"); u.println(i.read(0x77, BME280_CTRL_HUMIDITY_REG),HEX);
-	u.print("Temp MSB (0xFA): 0x"); u.println(i.read(0x77, 0xFA),HEX);
-	u.print("Temp LSB (0xFB): 0x"); u.println(i.read(0x77, 0xFB),HEX);
-	u.print("Temp XLSB (0xFC): 0x"); u.println(i.read(0x77, 0xFC),HEX);
+    u.print("ID (0xD0): 0x"); u.println(bme.read(0x77, BME280_CHIP_ID_REG),HEX);
+	u.print("Reset register (0xE0): 0x"); u.println(bme.read(0x77, BME280_RST_REG),HEX);
+	u.print("ctrl_meas (0xF4): 0x"); u.println(bme.read(0x77, BME280_CTRL_MEAS_REG),HEX);
+	u.print("ctrl_hum (0xF2): 0x"); u.println(bme.read(0x77, BME280_CTRL_HUMIDITY_REG),HEX);
+	u.print("Temp MSB (0xFA): 0x"); u.println(bme.read(0x77, 0xFA),HEX);
+	u.print("Temp LSB (0xFB): 0x"); u.println(bme.read(0x77, 0xFB),HEX);
+	u.print("Temp XLSB (0xFC): 0x"); u.println(bme.read(0x77, 0xFC),HEX);
 
-	u.print("DIG T1 MSB-LSB: 0x"); u.print(i.read(0x77, 0x89),HEX); u.println(i.read(0x77, 0x88),HEX);
-	u.print("DIG T2 MSB-LSB: 0x"); u.print(i.read(0x77, 0x8B),HEX); u.println(i.read(0x77, 0x8A),HEX);
-	u.print("DIG T3 MSB-LSB: 0x"); u.print(i.read(0x77, 0x8D),HEX); u.println(i.read(0x77, 0x8C),HEX);
+	u.print("DIG T1 MSB-LSB: 0x"); u.print(bme.read(0x77, 0x89),HEX); u.println(bme.read(0x77, 0x88),HEX);
+	u.print("DIG T2 MSB-LSB: 0x"); u.print(bme.read(0x77, 0x8B),HEX); u.println(bme.read(0x77, 0x8A),HEX);
+	u.print("DIG T3 MSB-LSB: 0x"); u.print(bme.read(0x77, 0x8D),HEX); u.println(bme.read(0x77, 0x8C),HEX);
 	
-	adct = (int32_t)(i.read(0x77, 0xFA)<<16)+ (int32_t)(i.read(0x77, 0xFB)<<8) + (int32_t)(i.read(0x77, 0xFC));
-	t1 = (int32_t)(i.read(0x77, 0x89)<<8)+ (int32_t)(i.read(0x77, 0x88));
-	t2 = (int32_t)(i.read(0x77, 0x8B)<<8)+ (int32_t)(i.read(0x77, 0x8A));
-	t3 = (int32_t)(i.read(0x77, 0x8D)<<8)+ (int32_t)(i.read(0x77, 0x8C));
+	adct = (int32_t)(bme.read(0x77, 0xFA)<<16)+ (int32_t)(bme.read(0x77, 0xFB)<<8) + (int32_t)(bme.read(0x77, 0xFC));
+	t1 = (int32_t)(bme.read(0x77, 0x89)<<8)+ (int32_t)(bme.read(0x77, 0x88));
+	t2 = (int32_t)(bme.read(0x77, 0x8B)<<8)+ (int32_t)(bme.read(0x77, 0x8A));
+	t3 = (int32_t)(bme.read(0x77, 0x8D)<<8)+ (int32_t)(bme.read(0x77, 0x8C));
 	u.print("adc : ");u.println(adct);
 	u.print("t1 : ");u.println(t1);
 	u.print("t2 : ");u.println(t2);
@@ -104,10 +108,10 @@ void read_sensor_test(void){
 	hasil = jadiTempC(adct, t1, t2, t3);
 	u.print("temperaturnya : ");u.println(hasil);
 	/*
-	printf("Press MSB (0xF7): 0x%x\n", i.read(0x77, 0xF7));
-	printf("Press LSB (0xF8): 0x%x\n", i.read(0x77, 0xF8));
-	printf("Press XLSB (0xF9): 0x%x\n", i.read(0x77, 0xF9));
-	printf("Hum MSB (0xFD): 0x%x\n", i.read(0x77, 0xFD));
-	printf("Hum LSB (0xFE): 0x%x\n", i.read(0x77, 0xFE));
+	printf("Press MSB (0xF7): 0x%x\n", bme.read(0x77, 0xF7));
+	printf("Press LSB (0xF8): 0x%x\n", bme.read(0x77, 0xF8));
+	printf("Press XLSB (0xF9): 0x%x\n", bme.read(0x77, 0xF9));
+	printf("Hum MSB (0xFD): 0x%x\n", bme.read(0x77, 0xFD));
+	printf("Hum LSB (0xFE): 0x%x\n", bme.read(0x77, 0xFE));
 	puts("Read BME280 Sensor Test Done");  */
 }
