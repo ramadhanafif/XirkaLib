d43846eb90775d37f451bd2bc1078c8cab7ec7df
ramadhanafif <31378187+ramadhanafif@users.noreply.github.com>
2019-11-21 13:37:37 +0700

    Update utility.h

--- utility/utility.h
+++ utility/utility.h
@@ -5,15 +5,16 @@
 #include "CMSDK_CM3.h"
 
 #ifdef __cplusplus
- extern "C" {
+extern "C" {
 #endif
-	void delaymilis(uint32_t);
- 	int  second(int);
-	void delaymicros(uint32_t);
-	void delay(uint32_t);
-	void delaysecond(int);
+unsigned int delaymilis(uint32_t ms);
+unsigned int delaysecond(uint32_t s);
+//void delaymicros(uint32_t);
+void delay(uint32_t);
+void millis_start(void);
+void SysTick_Handler(void);
+unsigned long millis(void);
 #ifdef __cplusplus
- }
+}
 #endif
 #endif //__UTILITY_H 
- 
