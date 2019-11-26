eb2ef0c32f90b7c7323738d0f14936a14b7503a0
ramadhanafif <ramadhanafif@gmail.com>
2019-11-26 17:19:41 +0700

    Update utility.c

--- utility/utility.c
+++ utility/utility.c
@@ -1,28 +1,42 @@
 #include "utility.h"
 
-volatile unsigned long _millis;
-volatile uint8_t __millis_active__ = 0;
+volatile unsigned long _millis = 0;
+volatile unsigned long _micros = 0;
+volatile unsigned int _mil_ = 0;
 
-void millis_start(void) {
-  SysTick_Config(SystemFrequency / 1000); //1KHz
-  __millis_active__ = 1;
+volatile uint8_t __timer_active__ = 0;
+
+void timer_start(void) {
+  SysTick_Config(20000000UL / 1000000); //1MHz
+  __timer_active__ = 1;
   _millis = 0;
 }
 
-
-void SysTick_Handler(void) {
-  _millis++;
+static void SysTick_Handler(void) {
+  _micros++;
+  _mil_++;
+  if(_mil_>=1000){
+    _mil_-=1000;
+    _millis++;
+  }
 }
 
 unsigned long millis(void) {
-  if (__millis_active__)
+  if (__timer_active__)
     return _millis;
   else
     return 0;
 }
 
+unsigned long micros(void) {
+  if (__timer_active__)
+    return _micros;
+  else
+    return 0;
+}
+
 unsigned int delaymilis(uint32_t ms) {
-  if (__millis_active__)
+  if (__timer_active__)
   {
     uint32_t end = millis() + ms;
     while (end > millis());
@@ -40,17 +54,20 @@ unsigned int delaysecond(uint32_t s)
   return delaymilis(ms);
 }
 
-/* Disabled */
-//void delaymicros(uint32_t n)                                                                    //delay function declaration
-//{
-//  int i, r;
+unsigned int delaymicros(uint32_t n)
+{
+  if (__timer_active__)
+  {
+    uint32_t end = micros() + n;
+    while (end > micros());
+    return n;
+  }
+  else
+  {
+    return 0;
+  }
+}
 
-//  for (i = 0; i < n; i++) {
-//    r = 2;
-//    while (r--);
-//  }//for
-//}
-
-void delay(uint32_t n) {
-  delaymilis(n);
+unsigned int delay(uint32_t n) {
+  return delaymilis(n);
 }
